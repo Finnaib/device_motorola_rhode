d86ce8ab4dea6f36fc8ed4c5aa78ce2d78c2f64e
Finnaib <shoaibwwe01@gmail.com>
2023-08-24 01:51:24 +0530

    rhode:AwakenOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The awakenOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/awaken_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rhode-user \
-    lineage_rhode-userdebug \
-    lineage_rhode-eng
+    awaken_rhode-user \
+    awaken_rhode-userdebug \
+    awaken_rhode-eng
--- awaken_rhode.mk
+++ awaken_rhode.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The awakenOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common awakenOS stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# GMS
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+
+# Extras
+AWAKEN_BUILD_TYPE := COMMUNITY
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_rhode
+PRODUCT_NAME := awaken_rhode
 PRODUCT_DEVICE := rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
